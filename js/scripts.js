7ab750c95ea16c4cde1f6941f7df99ffff8bb7a6
andreanunley <121556413+andreanunley@users.noreply.github.com>
2023-01-17 09:14:30 -0500

    Adds responsive mobile menu

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,12 @@ $('nav ul li a').on('click', function(){
 $('nav ul li a.active').removeClass('active');
 $(this).addClass('active');
 });
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
